464a2bea5ff13a805250db894f7b3736ff3b85b2
xFinVi <111379135+xFinVi@users.noreply.github.com>
2023-01-04 17:58:24 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     nav.classList.toggle('active')
 })
+
+
+
+
+const text = document.querySelectorAll('.ptext')
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry =>{
+            console.log(entry)
+            entry.target.classList.toggle('fade-in', entry.isIntersecting)
+            /* if (entry.isIntersecting) observer.unobserve(entry.target) */
+   })
+},
+{
+    rootMargin: "-150px"
+})
+
+text.forEach(fade => {
+    observer.observe(fade)
+})
+
